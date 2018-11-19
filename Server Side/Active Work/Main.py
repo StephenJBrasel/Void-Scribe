55c909bc1e614b55ae5c5c935aa52a44805162bf
Flounder (PC) <jmmyers1@student.fullsail.edu>
2018-11-19 09:19:18 -0500

    Main.py - This file includes the Mediator thread where threads will be spawned and interact with each other. This thread is also responsible for safe shut-down. Server_Start.py small - file that launches the server Terminal_Controller.py - Script that controls terminal input and command execution. Provides an interface for the adding of additional commands without the need to refactor. Commands are intended to be added in a decentralized manor through AddTerminalCommand function. - Help commands implemented - Exit commands implemented

---
+++ Server Side/Active Work/Main.py
@@ -0,0 +1,66 @@
+import threading
+import queue
+import time
+import Terminal_Controller
+
+
+main_processings = queue.Queue()
+shut_down_processes = queue.Queue()
+master_thread_list = []
+
+
+def SpinUpThread(target, name, daemon=False):
+    thread = threading.Thread(target=target, name=name)
+    thread.setDaemon(daemon)
+    thread.start()
+    master_thread_list.append(thread)
+
+def AddShutDownProcess(procces):
+    shut_down_processes.put(procces)
+    
+
+def MainEntryPoint():
+    
+    shut_down = False
+    #Terminal Command Definitions
+    #Exit Command
+    def command_exit():
+        nonlocal shut_down
+        shut_down = True
+    help_tip_exit = "Triggers the gracefull termination of the server."
+    def post_report_exit():
+        return None
+
+    #Start-Up Procedure
+    SpinUpThread(Terminal_Controller.InputEntryPoint, "Terminal Controller", True)
+    SpinUpThread(Terminal_Controller.ExecutionerEntryPoint, "Terminal Exeutioner")
+
+    Terminal_Controller.AddTerminalCommand("exit", command_exit, help_tip_exit, post_report_exit)
+
+    #Run-Time Procedure
+    while True:
+        if shut_down:
+            break
+
+        try:
+            process = main_processings.get_nowait()
+        except:
+            time.sleep(1/10)
+            continue
+
+        process()
+    
+    #Exit Proceedure
+    while True:
+        try:
+            exit_process = shut_down_processes.get_nowait()
+        except:
+            break
+    
+        exit_process()
+
+    for thread in master_thread_list:
+        if thread.is_alive() and not thread.isDaemon():
+            thread.join()
+
+    print("Gracefully Terminated")
