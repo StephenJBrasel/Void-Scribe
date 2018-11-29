ac596664055dd9605d0092bbe3c40b0378a1bcef
Stephen Brasel <stephen.brasel@gmail.com>
2018-11-29 13:09:01 -0500

    made relative path for names.csv

--- Server Side/Active Work/NameGenerator/nameGeneratorTest.py
+++
@@ -1,9 +0,0 @@
-import NameGenerator
-
-
-def main():
-    # print(NameGenerator.getNameTypes())
-    # print(NameGenerator.MarkovName('americanForenames'))
-    print(NameGenerator.getNames('breads'))
-
-main()
