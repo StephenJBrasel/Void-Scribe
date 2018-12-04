574f5e8b70d058062f89efc2b379f3765d8356af
forthelols69420 <forthelols69420@gmail.com>
2018-12-04 09:56:51 -0500

    FireStore_Monitor.py now works with Processed flag rather than timestamps

--- Server Side/Active Work/FireStore_Monitor.py
+++ Server Side/Active Work/FireStore_Monitor.py
@@ -1,28 +1,27 @@
 
-
-
 def FireStoreMonitorEntryPoint():
-    #Create a secure connection to the Firestore REST API using google Oauth2
-    from google.oauth2 import service_account
-    from google.auth.transport.requests import AuthorizedSession
-    scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/firebase.database"]
+    # #Create a secure connection to the Firestore REST API using google Oauth2
+    # from google.oauth2 import service_account
+    # from google.auth.transport.requests import AuthorizedSession
+    # scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/firebase.database"]
 
-    credentials = service_account.Credentials.from_service_account_file( r'C:\Users\thepe_000\Desktop\PP5\Void Scribe\Server Side\Active Work\void-scribe-firebase-adminsdk-xtf9j-a419db8670.json', scopes=scopes)
+    # credentials = service_account.Credentials.from_service_account_file( r'C:\Users\thepe_000\Desktop\PP5\Void Scribe\Server Side\Active Work\void-scribe-firebase-adminsdk-xtf9j-a419db8670.json', scopes=scopes)
 
-    authed_session = AuthorizedSession(credentials)
+    # authed_session = AuthorizedSession(credentials)
 
-    #Use authenticated session to retreive the current timestamp
-    import json
-    my_data = dict()
-    my_data["timestamp"] = {".sv": "timestamp"}
-    json_data = json.dumps(my_data).encode()
+    # #Use authenticated session to retreive the current timestamp
+    # import json
+    # my_data = dict()
+    # my_data["timestamp"] = {".sv": "timestamp"}
+    # json_data = json.dumps(my_data).encode()
 
-    response = authed_session.put("https://void-scribe.firebaseio.com/TEST_COLLECTION/TEST_DOCUMENT_2.json", data=json_data)
+    # response = authed_session.put("https://void-scribe.firebaseio.com/TEST_COLLECTION/TEST_DOCUMENT_2.json", data=json_data)
 
-    if response.status_code != 200:
-        raise Exception("Communication With The Firebase REST Server Unsuccesful")
+    # if response.status_code != 200:
+    #     raise Exception("Communication With The Firebase REST Server Unsuccesful")
 
-    last_timestamp = response.json()['timestamp']
+    # last_timestamp = response.json()['timestamp']
+
 
     #Establish database connection
     import firebase_admin
@@ -68,7 +67,7 @@ def FireStoreMonitorEntryPoint():
         if shut_down:
             break
 
-        query_ref = requests_ref.where(u'Timestamp', u'>', last_timestamp)
+        query_ref = requests_ref.where(u'Processed', u'==', False)
 
         doc_generator = query_ref.get()
 
@@ -76,10 +75,10 @@ def FireStoreMonitorEntryPoint():
             
             values = doc.to_dict()
 
-            if values['Timestamp'] > last_timestamp:
-                last_timestamp = values['Timestamp']
-
             Algorithm_Processor.EnqueueRequest(doc)
+            
+            doc.reference.update({'Processed':True})
+            
 
         time.sleep(query_rate)
 
