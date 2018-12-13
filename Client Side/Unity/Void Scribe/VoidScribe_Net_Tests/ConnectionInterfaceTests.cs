5ea58d7c5d2c5bebde69af17e08e710a50c59471
Flounder (PC) <jmmyers1@student.fullsail.edu>
2018-12-13 02:03:29 -0500

    Initial Commit of Unity Implementation

---
+++ Client Side/Unity/Void Scribe/VoidScribe_Net_Tests/ConnectionInterfaceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VoidScribe.Connection;
+using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using VoidScribe.Connection;
+
+namespace VoidScribe_Net_Tests
+{
+    [TestClass]
+    public class ConnectionInterfaceTests
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            FirestoreDb db = ConnectionInterface.Connect();
+
+            Assert.AreEqual("void-scribe", db.ProjectId);
+        }
+    }
+}
