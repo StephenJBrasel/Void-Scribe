8db95712d147b02a0ea14b25ee13eb7a48d43e51
forthelols69420 <forthelols69420@gmail.com>
2019-01-30 10:54:17 -0500

    Updated NamesDictionary and NamesTools.py with new functionalities to improve usability

--- MatainanceCode/NamesTools.py
+++ MatainanceCode/NamesTools.py
@@ -1,5 +1,251 @@
-import ..data.NamesDictionary
+import pickle
+import os
 
-names = data.NamesDictionary.NamesDictionary()
+data_path = "C:\\Users\\thepe_000\\Desktop\\PP5\\VoidScribe\\void_scribe\\data\\Names\\"
 
-print(names['americanCities'])
+def createIndex(path):
+        #helper function, yields all files in a directory
+        def files(path):  
+            for file in os.listdir(path):
+                if os.path.isfile(os.path.join(path, file)):
+                    yield file
+
+        index = {}
+        for file in files(path):
+            key = file.split('.')[0]
+            index[key] = path + file
+        return index
+
+def loadNameType(Name_Type, index):
+        return pickle.load(open(index[Name_Type], "rb" ))
+
+def writeNameType(data_path, new_Name_Type, new_Data, old_Name_Type=None):
+    # Writes a new pickled dictionairy object to data_path using new_Name_Type as the filename 
+    # and new_Data as the data
+    # if old_Name_Type is specified, the file will be removed
+
+    # Setup for deletion
+    to_del = ""
+    if old_Name_Type != None:
+        to_del = data_path + old_Name_Type
+    else:
+        to_del = data_path + new_Name_Type
+
+    # Check to ensure file is valid
+    if os.path.isfile(to_del):
+        # delete file
+        os.remove(to_del)
+
+    # Pickle new file
+    pickle.dump(new_Data, open(data_path + new_Name_Type + '.p', "wb"))
+
+def saveNameType(index, Name_Type, data):
+
+    to_del = data_path + Name_Type + '.p'
+    # Check to ensure file is valid
+    if os.path.isfile(to_del):
+        # delete file
+        os.remove(to_del)
+    pickle.dump(data, open(data_path + Name_Type + '.p', "wb"))
+
+def Console(options):
+    # Options is dictionary where:
+    #   key is name of the option
+    #   value is a function to call
+
+    def printOptions():
+        print("Select an option by entering a valid number.")
+        i = 0
+        for i, option in enumerate(options.keys()):
+            print(f'{i}. {option}')
+        print(f'{i + 1}. exit')
+
+    def getInput():
+        arg = input()
+        if arg == 'exit':
+            return arg
+        if arg not in options.keys():
+            return None
+        return arg
+
+    def loop():
+
+        while True:
+            printOptions()
+            option = getInput()
+            while option == None:
+                print("Invalid input, please re-enter")
+                option = getInput()
+            if option == 'exit':    
+                break
+            else:
+                options[option]()
+
+    loop()
+
+def search():
+
+    def searchForName_Type():
+        index = createIndex(data_path)
+        print('Please enter a valid Name_Type, or exit')
+        while True:
+            arg = input()
+            if arg == 'exit':
+                return
+            if arg not in index.keys():
+                print(f'{arg} Name_Type not found, please check spelling')
+                continue
+            break 
+
+        Name_Type = loadNameType(arg, index)
+
+        def Tags():
+            def view():
+                print(f"Name_Type: {arg} has the following tags.")
+                for tag in Name_Type['Tags']:
+                    print(tag)
+            def add():
+                print(f"Please enter a new tag for the {arg} Name_Type.")
+                new = input()
+                print(f"Adding Tag: {new}, confirm (y/n)")
+                confirm = input()
+                if confirm == 'y':
+                    Name_Type['Tags'].append(new)
+                    saveNameType(index, arg, Name_Type)
+                    print("New Tag Added")
+            def remove():
+                view()
+                print(f"Please enter a tag to remove from the {arg} Name_Type.")
+                rmv = input()
+                if rmv == 'exit':
+                    return
+                while rmv not in Name_Type['Tags']:
+                    print('Entered Tag not found, please check spelling or enter "exit"')
+                    rmv = input()
+                    if rmv == 'exit':
+                        return
+                print(f"Removing Tag: {rmv}, confirm (y/n)")
+                confirm = input()
+                if confirm == 'y':
+                    Name_Type['Tags'].remove(rmv)
+                    saveNameType(index, arg, Name_Type)
+                    print("Tag Deleted")
+            
+
+            options = {'view':view, 'add':add, 'remove':remove}
+            Console(options)
+
+        def Category():
+            def rename():
+                    print("Please enter a new name")
+                    new = input()
+                    cat = Name_Type["Category"]
+                    print(f"Replacing category name: {cat} with new category: {new}. Confirm? (y/n)")
+                    confirm = input()
+                    if confirm == 'y':
+                        Name_Type['Category'] = new
+                        saveNameType(index, arg, Name_Type)
+                        print("Category renamed")
+    
+            options = {'rename':rename}
+            print(f"Name_Type {arg} is categorized as {Name_Type['Category']}.")
+            Console(options)
+
+        options = {'Tags':Tags, 'Category':Category}
+        Console(options)
+
+
+    def searchByTag():
+        print('Gathering Tags...')
+
+        tag_data = {}
+        index = createIndex(data_path)
+        for key in index.keys():
+            for tag in loadNameType(key, index)['Tags']:
+                if tag not in tag_data.keys():
+                    tag_data[tag] = []
+                tag_data[tag].append(key)
+
+        print('Finished indexing tags, please enter a tag to search')
+        print(tag_data.keys())
+        arg = input()
+        while arg not in tag_data.keys():
+            print('Tag not found please enter a valid tag or exit')
+            arg = input()
+            if arg == 'exit':
+                break
+        print(f"Tag {arg} has the following associated Name_Types:")
+        for name_type in tag_data[arg]:
+            print(name_type)
+    
+    def searchByCategory():
+        print('Gathering Cateories...')
+
+        cat_data = {}
+        index = createIndex(data_path)
+        for key in index.keys():
+            cat = loadNameType(key, index)['Category']
+            if cat not in cat_data.keys():
+                cat_data[cat] = []
+            cat_data[cat].append(key)
+
+        print('Finished indexing Categories, please enter a Category to search')
+        print(cat_data.keys())
+        arg = input()
+        while arg not in cat_data.keys():
+            print('Category not found please enter a valid tag or exit')
+            arg = input()
+            if arg == 'exit':
+                break
+        print(f"Category {arg} has the following associated Name_Types:")
+        for name_type in cat_data[arg]:
+            print(name_type)
+
+    options = {'nameType':searchForName_Type, 'tag':searchByTag, 'category':searchByCategory}
+    Console(options)
+
+def Analysis():
+
+    def sumTags():
+        rates_of_apearance = {}
+        index = createIndex(data_path)
+        for key in index.keys():
+            for tag in loadNameType(key, index)['Tags']:
+                if tag not in rates_of_apearance.keys():
+                    rates_of_apearance[tag] = 1
+                else:
+                    rates_of_apearance[tag] += 1
+        
+        for key in rates_of_apearance.keys():
+            print(f"Tag {key} apears {rates_of_apearance[key]} times.")
+
+    def sumCategories():
+        rates_of_apearance = {}
+        index = createIndex(data_path)
+        for key in index.keys():
+            cat = loadNameType(key, index)['Category']
+            if cat not in rates_of_apearance.keys():
+                rates_of_apearance[cat] = 1
+            else:
+                rates_of_apearance[cat] += 1
+        
+        for key in rates_of_apearance.keys():
+            print(f"Category {key} apears {rates_of_apearance[key]} times.")
+
+    options = {'tags':sumTags, 'category':sumCategories}
+    Console(options)
+
+options = {'search':search, 'analysis':Analysis}
+
+if __name__ == '__main__':
+    Console(options)
+
+
+
+    
+
+    
+        
+
+    
+
--- void_scribe/data/NamesDictionary.py
+++ void_scribe/data/NamesDictionary.py
@@ -4,20 +4,42 @@ import os
 
 class NamesDictionary:
     def __init__(self):
-        #__DATA_PATH__ is the folder in which pickled name files are stored
+        # __DATA_PATH__ is the folder in which pickled name files are stored
         self.__DATA_PATH__ = pkg_resources.resource_filename('void_scribe', 'data/Names/')
-        #__index__ maps each Name_Type string to the filepath of its pickle data
+        # __index__ maps each Name_Type string to the filepath of its pickle data
         self.__index__ = self.__createIndex__(self.__DATA_PATH__)
+        # __requiredKeys__ is a list of keys that are required of incoming Name_Types
+        self.__requiredKeys__ = ['Category', 'Tags', 'Data']
 
     def __loadNameType__(self, Name_Type):
 
         return pickle.load(open(self.__index__[Name_Type], "rb" ))
+
+    def __saveNameType__(self, name_type, data):
+        if type(data) != type({}):
+            raise ValueError("Argument 'data' must be of type {}, passed argument is of type {}.".format(type({}), type(data)))
+        if type(name_type) != type(""):
+            raise ValueError("Argument 'name_type' must be of type {}, passed argument is of type {}.".format(type(""), type(name_type)))
+        pickle.dump(data, open(self.__DATA_PATH__ + name_type + '.p', "wb"))
+        # Update Index
+        self.__updateIndex__()
+
+    def __verifyNameType__(self, data):
+        if type(data) != type({}):
+            raise ValueError("Argument data must be of type {}, passed argument is of type {}.".format(type({}), type(data)))
+        for req in self.__requiredKeys__:
+            if req not in data.keys():
+                return req
+        return True
 
     def __getitem__(self, key):
         if key not in self.keys():
             raise KeyError(f"Provided key: '{key}' is not a valid Name_Type.")
 
         return self.__loadNameType__(key)
+
+    def __len__(self):
+        return len(self.__index__)
 
     def __createIndex__(self, path):
         #helper function, yields all files in a directory
@@ -32,6 +54,18 @@ class NamesDictionary:
             index[key] = path + file
         return index
 
+    def __updateIndex__(self):
+        self.__index__ = self.__createIndex__(self.__DATA_PATH__)
+
+    def __setitem__(self, key, value):
+        self.update(value, key)
+
+    def __delitem__(self, key):
+        self.remove(key)
+
+    def __iter__(self):
+        return self.__index__.__iter__()
+
     def keys(self):
         return list(self.__index__.keys())
 
@@ -42,6 +76,91 @@ class NamesDictionary:
     def values(self):
         for key in self.keys():
             yield self.__getitem__(key)
+
+    def update(self, data, name_type, overwrite=False):
+        # Updates the NamesDictionary with the given "other"
+        # data must be either a dictionary or an iterable with key/values pairs
+        # either as tuples or iterables of length 2
+        # Returns the key of the dictionary if one was added, returns None if no update was made.
+        # If a entry already exists under the provided name_type an execption is raised,
+        # unless overwrite is set to True. In this case the existing entry is overwritten NOT merged.
+        # data must contain the following keys with the associatly typed value,
+        # otherwise an execption is raised.
+        # Additional keys are ignored.
+        #
+        #   {
+        #       'Category':'CATEGORY_NAME',  
+        #       'Tags':[ITERABLE OF TYPE STRING],
+        #       'Data':[ITERABLE OF TYPE STRING]
+        #   }
+
+        if data == None:
+            raise ValueError("Missing required argument: 'data'.")
+        elif name_type == None:
+            raise ValueError("Missing required argument: 'name_type'.")
+        elif type(name_type) != type(""):
+            raise ValueError("Argument 'name_type' must be of type {} not of type {} .".format(type(""), type(name_type)))
+
+        if len(data) == 0:
+            return None
+
+        if overwrite == False and name_type in self.keys():
+            raise KeyError("Key {} already exists and 'overwrite' is not enabled. To enbale overwrite use the update() function.".format(name_type))
+
+        save = {}
+
+        # Build the save object
+        if type(data) == type({}):
+            # data is a dictionairy
+            # Check for required keys
+            res = self.__verifyNameType__(data)
+            if res != True:
+                raise ValueError("Argument 'data' was missing required key {}.".format(res))
+            # build save object
+            for req in self.__requiredKeys__:
+                save[req] = data[req]
+        elif '__iter__' not in dir(data):
+            raise ValueError("Argument 'data' must be of type {} or be iterable.".format(type({})))
+        else:
+            # data is an iterable
+            # Check for contents to be iterble
+            if '__iter__' not in dir(data[0]):
+                raise ValueError("Argument 'data' was an iterable but contains non-iterable key/value pairs")
+            # Check for length two componets.
+            elif len(data[0]) != 2:
+                raise ValueError("Argument 'data' was an iterable but contains iterable objects that are not length 2.")
+            temp = {}
+            # Build a dictionary from k/v pairs.
+            for pair in data:
+                temp[pair[0]] = pair[1]
+            # Check for required keys.
+            res = self.__verifyNameType__(temp)
+            if res != True:
+                raise ValueError("Argument 'data' was missing required key {}.".format(res))
+            # Build save object.
+            for req in self.__requiredKeys__:
+                save[req] = temp[req]  
+        # Save Object
+        self.__saveNameType__(name_type, save)
+            
+    def remove(self, name_type):
+        # Returns True if an entry was removed, False if not found
+        # CAUTION WILL DELETE DATA FROM DISK
+        if type(name_type) != type(""):
+            raise ValueError("Argument 'name_type' was not required type: {}, 'name_type' is of type: {}.".format(type(""), type(name_type)))
+        elif name_type not in self.keys():
+            return False
+        else:
+            # Delete pickle file
+            os.remove(self.__index__[name_type])
+            # Update index
+            self.__updateIndex__()
+
+
+            
+        
+        
+
 
             
 
