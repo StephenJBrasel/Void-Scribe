4a60ed38b22b6f67187dbb5e51b141cc63313fbd
Stephen Brasel <wearyroad@gmail.com>
2019-02-05 23:23:18 -0500

    trimmed the placename data, introduced prompt generation.

--- void_scribe/PromptGenerator.py
+++ void_scribe/PromptGenerator.py
@@ -3,7 +3,12 @@ import random
 import pickle
 import requests
 import pkg_resources
-import nlglib
+from nlglib.realisation.simplenlg.realisation import Realiser
+from nlglib.realisation.simplenlg.realisation import BasicRealiser
+from nlglib.microplanning import *
+
+# realise_en = Realiser(host='nlg.kutlak.info', port=40000)
+realise_en = BasicRealiser()
 
 def loadPickle(data = None, fileName = "ActionVerbs.p"):
     path = pkg_resources.resource_filename('void_scribe', 'data/')
@@ -16,15 +21,19 @@ def generatePrompt(seed = None):
     random.seed(seed)
     ActionVerbs = loadPickle()
     currentVerb = random.choice(ActionVerbs)
-    subject = NameGenerator.getNames(amount = 1)[0]
-    direct_object = NameGenerator.getNames(amount = 1)[0]
+    subject = NameGenerator.getNames(amount = 1)[0].capitalize()
+    direct_object = NameGenerator.getNames(amount = 1)[0].capitalize()
+    p = Clause(NP(subject), VP(currentVerb), NP(direct_object))
+    ret = realise_en(p)
     print(f"{subject} {currentVerb} {direct_object}.")
-    # print(currentVerb)
+    print(ret)
     # obj = requests.get("http://api.conceptnet.io/c/en/" + currentVerb).json()
     # for key in obj['edges']:
     #     print(f'{key}')
     #     print()
     # print(currentVerb)
+    return ret
+
 
 if __name__ == "__main__":
     generatePrompt()
