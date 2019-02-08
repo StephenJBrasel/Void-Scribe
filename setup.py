825f4635a234046c83df09c3108fa6be46f31f3c
Flounder (PC) <jmmyers1@student.fullsail.edu>
2019-02-08 12:59:50 -0500

    Updated Manifest.in and setup.py while debugging following removed files are the source of the bug, filenames cannot contain non ASCII characters

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 from distutils.core import setup
 
-#This is a list of files to install, and where
-#(relative to the 'root' dir, where setup.py is)
-#You could be more specific.
-files = ["data/*"]
+files = ["voids_cribe/data/*", "voids_cribe/data/Names/*", "voids_cribe/data/MarkovDictionaries/*"]
 
 setup(name = "void_scribe",
     version = "0.1a",
     description = "Procedural Natural Language Generation",
-    author = "Stephen Brasel",
-    author_email = "machinelearningmadlads@gmail.com",
-    url = "https://github.com/GermanMoFo/Void-Scribe",
-    #Name the folder where your packages live:
-    #(If you have other packages (dirs) or modules (py files) then
-    #put them into the package directory - they will be found 
-    #recursively.)
+    author = "Stephen Brasel, Joshua Myers",
     packages = ['void_scribe'],
-    #'package' package must contain files (see list above)
-    #I called the package 'package' thus cleverly confusing the whole issue...
-    #This dict maps the package name =to=> directories
-    #It says, package *needs* these files.
-    package_data = {'void_scribe' : files },
+    package_dir = {'void_scribe':'void_scribe'},
     include_package_data=True
-    #'runner' is in the root.
-    #scripts = ["runner"],
-    #
-    #This next part is for the Cheese Shop, look a little down the page.
-    #classifiers = []     
 ) 
