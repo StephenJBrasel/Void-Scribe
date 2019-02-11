ba0d2a85378d8c2b85865363cd623d7364125394
forthelols69420 <forthelols69420@gmail.com>
2019-02-11 09:36:29 -0500

    Test of modified __init__.py, may roll change back if this does not work.

--- void_scribe/__init__.py
+++ void_scribe/__init__.py
@@ -1,1 +1,6 @@
 name = "void_scribe"
+import void_scribe.NameGenerator
+import void_scribe.NamesDictionary
+import void_scribe.PromptGenerator
+import void_scribe.MarkovGen
+import void_scribe.MarkovIndex
