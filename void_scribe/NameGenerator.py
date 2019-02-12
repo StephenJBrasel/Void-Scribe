6c50a1b7305525676fcce148f3cf58d03bd23d08
forthelols69420 <forthelols69420@gmail.com>
2019-02-12 13:40:07 -0500

    Fixed a bug causing NameGenerator.py to return lists of charecters rather than complete string from generateMarkovNames

--- void_scribe/NameGenerator.py
+++ void_scribe/NameGenerator.py
@@ -4,7 +4,10 @@ MI = MarkovIndex()
 
 def generateMarkovNames(Name_Type = None, amount = 1):
     namesMarkovDictionary = MI[Name_Type]
-    return MarkovGen.markovGenerate(namesMarkovDictionary, 3, amount)
+    names = MarkovGen.markovGenerate(namesMarkovDictionary, 3, amount)
+    for i in range(0, len(names)):
+        names[i] = "".join(names[i])
+    return names
 
 def realNames(Name_Type = 'americanForenames', amount = 1):
     from void_scribe import NamesDictionary
@@ -17,3 +20,5 @@ def realNames(Name_Type = 'americanForenames', amount = 1):
 
 def validNameTypes():
     return MI.keys()
+
+print(generateMarkovNames('pokemon', 5))
