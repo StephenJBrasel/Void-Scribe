0af96b5cf23dd6ee7ffa9bfb1a11ce2fde057c29
Flounder (PC) <jmmyers1@student.fullsail.edu>
2019-02-11 23:48:40 -0500

    fixing bug in __init__

--- void_scribe/__init__.py
+++ void_scribe/__init__.py
@@ -1,4 +1,3 @@
 name = "void_scribe"
 from void_scribe.NamesDictionary import NamesDictionary as NamesDictionary
 from void_scribe.MarkovIndex import MarkovIndex as MarkovIndex
-from NameGenerator import generateMarkovNames
