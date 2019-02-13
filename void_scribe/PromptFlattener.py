e4ba334a793b4cb790f9c1f35e587e1c18d9d506
forthelols69420 <forthelols69420@gmail.com>
2019-02-13 10:02:28 -0500

    Updated PromptFlattener.py to include PromptIndex, a re-implementation of the MArkovIndex/NamesDictionary designed for use with Prompt JSON files.

--- void_scribe/PromptFlattener.py
+++ void_scribe/PromptFlattener.py
@@ -3,17 +3,12 @@ from nlglib.microplanning import *
 import json
 import random
 from void_scribe import NameGenerator
+import pkg_resources
+import os
 
 realise = Realiser(host='nlg.kutlak.info')
 
 leadExamplePath = 'C:/Users/thepe_000/Desktop/PP5/Void-Scribe/void_scribe/data/PromptTemplates/lead.json'
-
-def loadPromptFromJSON(filepath):
-    # Loads a given json file and returns the data.
-    # filepath specifies the location of the file.
-    with open(filepath) as f:
-        data = json.load(f)
-    return data
 
 def selectNameTypeAndGenerateWord(nameTypes):
     # Selects a random NameType from the list passed, then calls for generation.
@@ -52,13 +47,20 @@ def generateElement(elementJSON):
         
 def contructClauseArguments(promptJSON):
     # Given a prompt JSON object this will flatten it.
-    # Loops through each element, then appends features
+    # Loops through each element, then appends features and complements
     clauseConstructorArguments = {}
     for argument in promptJSON['phrase'].keys():
         elementData = promptJSON['phrase'][argument]
         clauseConstructorArguments[argument] = generateElement(elementData)
     if 'features' in promptJSON.keys():
         clauseConstructorArguments['features'] = promptJSON['features']
+    if 'complements' in promptJSON.keys():
+        complements = []
+        for complementElementData in promptJSON['complements']:
+            complementElement = generateElement(complementElementData)
+            complements.append(complementElement)
+        clauseConstructorArguments['complements'] = complements
+
     return clauseConstructorArguments
 
 def realisePrompt(clauseArguments):
@@ -67,3 +69,72 @@ def realisePrompt(clauseArguments):
     clause = Clause(**clauseArguments)
 
     return realise(clause)
+
+class PromptIndex():
+    def __init__(self):
+        # __DATA_PATH__ is the folder in which pickled name files are stored
+        self.__DATA_PATH__ = pkg_resources.resource_filename('void_scribe', 'data/PromptTemplates/')
+        # __index__ maps each Prompt_Type string to the filepath of its pickle data
+        self.__index__ = self.__createIndex__(self.__DATA_PATH__)
+
+    def __loadPromptJSON__(self, Prompt_Type):
+        # Loads a given json file and returns the data.
+        # filepath specifies the location of the file.
+        with open(self.__index__[Prompt_Type]) as f:
+            data = json.load(f)
+        return data
+
+    def __getitem__(self, key):
+        if key not in self.keys():
+            raise KeyError(f"Provided key: '{key}' is not a valid Prompt_Type, or a dictionary for the Prompt_Type does not exist.")
+
+        return self.__loadPromptJSON__(key)
+
+    def __len__(self):
+        return len(self.__index__)
+
+    def __createIndex__(self, path):
+        #helper function, yields all files in a directory
+        def files(path):  
+            for file in os.listdir(path):
+                if os.path.isfile(os.path.join(path, file)):
+                    yield file
+
+        index = {}
+        for file in files(path):
+            key = file.split('.')[0]
+            index[key] = path + file
+        return index
+
+    def __updateIndex__(self):
+        self.__index__ = self.__createIndex__(self.__DATA_PATH__)
+
+    def __delitem__(self, key):
+        self.remove(key)
+
+    def __iter__(self):
+        return self.__index__.__iter__()
+
+    def keys(self):
+        return list(self.__index__.keys())
+
+    def items(self):
+        for key in self.keys():
+            yield (key, self.__getitem__(key))
+
+    def values(self):
+        for key in self.keys():
+            yield self.__getitem__(key)
+            
+    def remove(self, prompt_type):
+        # Returns True if an entry was removed, False if not found
+        # CAUTION WILL DELETE DATA FROM DISK
+        if type(prompt_type) != type(""):
+            raise ValueError("Argument 'prompt_type' was not required type: {}, 'prompt_type' is of type: {}.".format(type(""), type(prompt_type)))
+        elif prompt_type not in self.keys():
+            return False
+        else:
+            # Delete pickle file
+            os.remove(self.__index__[prompt_type])
+            # Update index
+            self.__updateIndex__()
