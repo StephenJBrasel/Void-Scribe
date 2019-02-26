463a795300f51ab248eb473a276de537118e14af
Flounder (PC) <jmmyers1@student.fullsail.edu>
2019-02-25 22:15:48 -0500

    Updated PromptFlattener.py to utalize data from void-web

--- void_scribe/PromptFlattener.py
+++ void_scribe/PromptFlattener.py
@@ -11,7 +11,8 @@ realise = Realiser(host='nlg.kutlak.info')
 def selectNameTypeAndGenerateWord(nameTypes):
     # Selects a random NameType from the list passed, then calls for generation.
     # Currently utilizes realNames over generateMarkovNames
-    nameGenerator = void_scribe.NameGenerator.NameGenerator()
+    from void_scribe.NameGenerator import NameGenerator
+    nameGenerator = NameGenerator()
     chosenNameType = random.randint(0, len(nameTypes) - 1)
     chosenNameType = nameTypes[chosenNameType]
     generatedWord = nameGenerator.retreiveNames(chosenNameType, 1)[0]
@@ -93,13 +94,19 @@ def realiseClause(clauseArguments):
     return realise(clause)
 
 def generatePrompt(promptType):
-    # Takes a promptType, loads associated JSON file
+    # Takes a promptType, loads associated JSON file from Void-Web
     # Creates componet dictionary
     # Runs argument and realization for each clause
     # joins clauses
-    PI = PromptIndex()
-    #Load prompt JSON from PromptIndex
-    promptJSON = PI[promptType]
+    from requests import post
+    url = 'http://www.voidscribe.com//data/prompts'
+    json = {"promptType":promptType}
+    resp = post(url=url, json=json)
+
+    if resp.status_code != 200:
+        raise Exception('Encountered Network Error With Void-Web when requesting prompt type data')
+
+    promptJSON = resp.json()
     
     componetDictionary = generateComponetDictionary(promptJSON['componets'])
     clauses = []
@@ -109,72 +116,5 @@ def generatePrompt(promptType):
         clauses.append(clause)
     return " ".join(clauses)
 
-class PromptIndex():
-    def __init__(self):
-        # __DATA_PATH__ is the folder in which pickled name files are stored
-        self.__DATA_PATH__ = pkg_resources.resource_filename('void_scribe', 'data/PromptTemplates/')
-        # __index__ maps each Prompt_Type string to the filepath of its pickle data
-        self.__index__ = self.__createIndex__(self.__DATA_PATH__)
-
-    def __loadPromptJSON__(self, Prompt_Type):
-        # Loads a given json file and returns the data.
-        # filepath specifies the location of the file.
-        with open(self.__index__[Prompt_Type]) as f:
-            data = json.load(f)
-        return data
-
-    def __getitem__(self, key):
-        if key not in self.keys():
-            raise KeyError(f"Provided key: '{key}' is not a valid Prompt_Type, or a dictionary for the Prompt_Type does not exist.")
-
-        return self.__loadPromptJSON__(key)
-
-    def __len__(self):
-        return len(self.__index__)
-
-    def __createIndex__(self, path):
-        #helper function, yields all files in a directory
-        def files(path):  
-            for file in os.listdir(path):
-                if os.path.isfile(os.path.join(path, file)):
-                    yield file
-
-        index = {}
-        for file in files(path):
-            key = file.split('.')[0]
-            index[key] = path + file
-        return index
-
-    def __updateIndex__(self):
-        self.__index__ = self.__createIndex__(self.__DATA_PATH__)
-
-    def __delitem__(self, key):
-        self.remove(key)
-
-    def __iter__(self):
-        return self.__index__.__iter__()
-
-    def keys(self):
-        return list(self.__index__.keys())
-
-    def items(self):
-        for key in self.keys():
-            yield (key, self.__getitem__(key))
-
-    def values(self):
-        for key in self.keys():
-            yield self.__getitem__(key)
-            
-    def remove(self, prompt_type):
-        # Returns True if an entry was removed, False if not found
-        # CAUTION WILL DELETE DATA FROM DISK
-        if type(prompt_type) != type(""):
-            raise ValueError("Argument 'prompt_type' was not required type: {}, 'prompt_type' is of type: {}.".format(type(""), type(prompt_type)))
-        elif prompt_type not in self.keys():
-            return False
-        else:
-            # Delete pickle file
-            os.remove(self.__index__[prompt_type])
-            # Update index
-            self.__updateIndex__()
+print(generatePrompt('acquire'))
 
